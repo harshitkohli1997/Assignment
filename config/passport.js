e05f23be627e065a7e53b043ee106609bc3025a3
harshitkohli1997 <harshit.kohli1997@gmail.com>
2019-04-08 02:40:16 +0530

    passport session added

--- config/passport.js
+++ config/passport.js
@@ -1,48 +1,39 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+const LocalStrategy  = require('passport-local').Strategy;
 const mongoose = require('mongoose');
-const User = mongoose.model('users');
 const bcrypt = require('bcryptjs');
 
-passport.use(new LocalStrategy({usernameField: 'email'}, (email, password, done) => {
-  // Match user
-  User.findOne({
-    email:email
-  }).then(user => {
-    if(!user){
-      return done(null, false, {message: 'No User Found'});
-    } 
+// Load user model
+const User = mongoose.model('users');
 
-    // Match password
-    bcrypt.compare(password, user.password, (err, isMatch) => {
-      if(err) throw err;
-      if(isMatch){
-        return done(null, user);
-      } else {
-        return done(null, false, {message: 'Password Incorrect'});
-      }
+module.exports = function(passport){
+  passport.use(new LocalStrategy({usernameField: 'email'}, (email, password, done) => {
+    // Match user
+    User.findOne({
+      email:email
+    }).then(user => {
+      if(!user){
+        return done(null, false, {message: 'No User Found'});
+      } 
+
+      // Match password
+      bcrypt.compare(password, user.password, (err, isMatch) => {
+        if(err) throw err;
+        if(isMatch){
+          return done(null, user);
+        } else {
+          return done(null, false, {message: 'Password Incorrect'});
+        }
+      })
     })
-  })
-}));
+  }));
 
-
-const passportJWT = require("passport-jwt");
-const JWTStrategy   = passportJWT.Strategy;
-const ExtractJWT = passportJWT.ExtractJwt;
-
-passport.use(new JWTStrategy({
-        jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-        secretOrKey   : 'your_jwt_secret'
-    },
-    function (jwtPayload, cb) {
-
-        //find the user in db if needed. This functionality may be omitted if you store everything you'll need in JWT payload.
-        return UserModel.findOneById(jwtPayload.id)
-            .then(user => {
-                return cb(null, user);
-            })
-            .catch(err => {
-                return cb(err);
-            });
-    }
-));
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+}
