f6be43878599d02f310868f543049657fd0b7c2d
harshitkohli1997 <harshit.kohli1997@gmail.com>
2019-04-08 00:08:02 +0530

    users added

---
+++ routes/users.js
@@ -0,0 +1,132 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const keys = require('../config/keys');
+const router = express.Router();
+
+// Load User Model
+require('../models/User');
+const User = mongoose.model('users');
+
+// User Login Route
+router.get('/login', (req, res) => {
+  res.render('users/login');
+});
+
+// User Register Route
+router.get('/register', (req, res) => {
+  res.render('users/register');
+});
+
+// Login Form POST
+router.post('/login', (req, res) => {
+  //const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check Validation
+  //if (!isValid) {
+    //return res.status(400).json(errors);
+  //}
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ email }).then(user => {
+    // Check for user
+    if (!user) {
+      errors.email = 'User not found';
+      return res.status(404).json(errors);
+    }
+
+    // Check Password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        // User Matched
+        const payload = { id: user.id, name: user.name, avatar: user.avatar }; // Create JWT Payload
+
+        // Sign Token
+        var auth = {}
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            auth.token = token;
+            auth.status = true;
+            res.render('ideas/index', {
+              auth:auth
+            });
+          }
+        );
+      } else {
+        errors.password = 'Password incorrect';
+        return res.status(400).json(errors);
+      }
+    });
+  });
+});
+
+
+// Register Form POST
+router.post('/register', (req, res) => {
+  let errors = [];
+
+  if(req.body.password != req.body.password2){
+    errors.push({text:'Passwords do not match'});
+  }
+
+  if(req.body.password.length < 4){
+    errors.push({text:'Password must be at least 4 characters'});
+  }
+
+  if(errors.length > 0){
+    res.render('users/register', {
+      errors: errors,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      password2: req.body.password2
+    });
+  } else {
+    User.findOne({email: req.body.email})
+      .then(user => {
+        if(user){
+          req.flash('error_msg', 'Email already regsitered');
+          res.redirect('/users/register');
+        } else {
+          const newUser = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+          });
+          
+          bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+              if(err) throw err;
+              newUser.password = hash;
+              newUser.save()
+                .then(user => {
+                  req.flash('success_msg', 'You are now registered and can log in');
+                  res.redirect('/users/login');
+                })
+                .catch(err => {
+                  console.log(err);
+                  return;
+                });
+            });
+          });
+        }
+      });
+  }
+});
+
+// Logout User
+router.get('/logout', (req, res) => {
+  req.logout();
+  req.flash('success_msg', 'You are logged out');
+  res.redirect('/users/login');
+});
+
+module.exports = router;
