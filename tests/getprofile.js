0feff12e59f646848de8e915d43270a7cbc4efbc
harshitkohli1997 <harshit.kohli1997@gmail.com>
2019-04-08 17:15:35 +0530

    final

--- tests/getprofile.js
+++ tests/getprofile.js
@@ -4,11 +4,14 @@ var request = require('supertest');
 const mongoose = require('mongoose');
 const User = mongoose.model('users');
 
+//dummy user for login
 const userCredentials = {
-    email: 'harshit.kohli1997@gmail.com', 
-    password: 'scooby1234'
+    email: 'abc@gmail.com', 
+    password: '12345'
   }
-  //now let's login the user before we run any tests
+
+
+
   var authenticatedUser = request.agent(app);
   beforeEach(function(done){
     authenticatedUser
@@ -20,6 +23,9 @@ const userCredentials = {
         done();
       });
   });
+
+//dummy user for register testing
+
   const newuser = {
     email:'test@gmail.com',
     name:'testing',
@@ -47,12 +53,11 @@ const userCredentials = {
 })
 
   describe('Test auth', function(done){
-    //addresses 1st bullet point: if the user is logged in we should get a 200 status code
       it('should return a 200 response if the user is logged in', function(done){
         authenticatedUser.get('/ideas')
         .expect(200, done);
       });
-    //addresses 2nd bullet point: if the user is not logged in we should get a 302 response code and be directed to the /login page
+    
       it('should return a 302 response and redirect to /login', function(done){
         request(app).get('/ideas')
         .expect('Location', '/users/login')
@@ -64,10 +69,10 @@ const userCredentials = {
       details:'updated details'
     }
     describe('test for update', function(done){
-      //addresses 1st bullet point: if the user is logged in we should get a 200 status code
+      
         it('should return a 200 response if the idea is updated', function(done){
           authenticatedUser
-          .put('/ideas/edit/5cab2be379335e369015729c')
+          .put('/ideas/edit/5cab161de9f32b2be0536a70')
           .send(idea)
           .expect(302, done);
           
